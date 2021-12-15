bd2ac6bacc0aef7a346cff27bd1b1768ea05be95
xoriston <1420damian@gmail.com>
2021-12-15 15:21:25 +0100

    testing new features

--- app.js
+++ app.js
@@ -13,9 +13,5 @@ import {
 const damian = new User("Damian", "Cur")
 const alchemik = new Book("Alchemik", "Paullo Coello", 1)
 const bookingDamian = new Booking(damian)
-
-console.log(bookingDamian);
-
-// var result = new Date();
-// result.setDate(result.getDate() + -7);
-// console.log(result);
+// console.log(new Date().getTime() + 86400000 * 7);
+bookingDamian.returnBook(alchemik)
